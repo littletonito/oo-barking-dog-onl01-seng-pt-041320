6be36829e38ea46d8b56f647340733ec59f22543
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-30 00:54:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+
+class Dog
+  
+  def name= (dog)
+   @doggies = dog
+  end
+  
+  
+  def name
+   @doggies
+   
+  end
+  
+  
+  def bark 
+    puts "woof!"
+  end
+
+
+end
